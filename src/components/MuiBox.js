f6a1d4bae286c1c7797b6d1a2facd84c79048c32
prasankumar <prasankumarr17@gmail.com>
2022-12-10 21:56:04 +0530

    Grid Mui

--- src/components/MuiBox.js
+++ src/components/MuiBox.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Stack, Divider } from "@mui/material";
+import { Box, Stack, Divider, Grid } from "@mui/material";
 
 const MuiBox = () => {
   return (
@@ -28,6 +28,54 @@ const MuiBox = () => {
         <Box height="100px" width="100px" bgcolor="success.light"></Box>
         <Box height="100px" width="100px" bgcolor="secondary.light"></Box>
       </Stack>
+
+      {/* Grid  */}
+      <Grid container my={4} spacing={2}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Box bgcolor="success.light" p={2}>
+            Item 1
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Box bgcolor="success.light" p={2}>
+            Item 2
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Box bgcolor="success.light" p={2}>
+            Item 3
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Box bgcolor="success.light" p={2}>
+            Item 4
+          </Box>
+        </Grid>
+      </Grid>
+
+      {/* Grid  */}
+      <Grid container my={4} rowSpacing={2} columnSpacing={1}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Box bgcolor="success.light" p={2}>
+            Item 1
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Box bgcolor="success.light" p={2}>
+            Item 2
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Box bgcolor="success.light" p={2}>
+            Item 3
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Box bgcolor="success.light" p={2}>
+            Item 4
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
